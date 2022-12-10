55508f4877361b6a32b995da1178f5b63bf5db3e
Sory02 <sorinnoroc1@gmail.com>
2022-12-10 15:11:51 +0200

    Fixed app icon

--- Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
+++ Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.set
 
 class MainActivity : AppCompatActivity() {
 
